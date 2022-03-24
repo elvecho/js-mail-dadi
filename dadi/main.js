59583f2c846e45d9aeaf32f692e48c01f1742263
elvecho <100934190+elvecho@users.noreply.github.com>
2022-03-24 13:55:19 +0100

    commit finale

---
+++ dadi/main.js
@@ -0,0 +1,12 @@
+let numerocomputer = Math.floor(Math.random()*6);
+console.log(numerocomputer);
+let numerogiocatore = Math.floor(Math.random()*6);
+console.log(numerogiocatore);
+
+if (numerocomputer>numerogiocatore){
+    console.log("ha vinto il computer");
+}else if(numerocomputer<numerogiocatore){
+    console.log("ha vinto il giocatore");
+}else{
+    console.log("pareggio");
+}
