f0ba49a82f92125ee845a261cf8ab45f72a755de
elvecho <100934190+elvecho@users.noreply.github.com>
2022-03-24 13:15:33 +0100

    esercizio email

---
+++ main.js
@@ -0,0 +1,10 @@
+let email= prompt("inserisci la tua email");
+let listaemail = ["matteo@gmail.com","fabio@gmail.com","pietro@gmail.com","anna@gmail.com"];
+
+console.log(listaemail);
+
+for( let i =0; i < listaemail.length; i++){
+    if(email == listaemail[i]){
+        console.log("la tua email è accettata");
+    }
+}
