12f20a867bd478bdd6a4ac677fae64d41a593e08
elvecho <100934190+elvecho@users.noreply.github.com>
2022-03-25 10:04:34 +0100

    corretto un po

--- email/main.js
+++ email/main.js
@@ -12,9 +12,15 @@ for( let i =0; i <= listaemail.length -1; i++){
     console.log("la tua email è accettata");
 }else{
     console.log("la tua email non è accettata");
-    let nuovaemail= prompt("vuoi inserire una nuova email?"); 
-    listaemail.push(nuovaemail);
+    let registrazione= confirm("vuoi inserire una nuova email?");
+if (registrazione == true){
+let nuovaemail = prompt("inserisci nuova email");
+listaemail.push(nuovaemail);
 console.log(listaemail);
+}else{
+    console.log("okay addio");
+}
+   
 }
 
 /*for( let i =0; i < listaemail.length; i++){
